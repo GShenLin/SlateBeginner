6d82f9f270b16dfc06e1a03b441017be31e60a0b
GSL <3303452085@qq.com>
2024-09-16 02:48:38 +0800

    FSlateApplication

--- Plugins/SlateExercise/Source/SlateExercise/Private/SlateExercise.cpp
+++ Plugins/SlateExercise/Source/SlateExercise/Private/SlateExercise.cpp
@@ -48,7 +48,7 @@ void FSlateExerciseModule::StartupModule()
 	// 创建命令 命令可以插入各种槽位中 表示点击后是什么样一个操作 响应的方式 比如像按钮一样
 	PluginCommands = MakeShareable(new FUICommandList);
 
-	// 绑定命令
+	// 绑定插件按钮点击时的命令
 	PluginCommands->MapAction(
 		FSlateExerciseCommands::Get().OpenPluginWindow,
 		FExecuteAction::CreateRaw(this, &FSlateExerciseModule::PluginButtonClicked),
@@ -56,12 +56,12 @@ void FSlateExerciseModule::StartupModule()
 
 	UToolMenus::RegisterStartupCallback(FSimpleMulticastDelegate::FDelegate::CreateRaw(this, &FSlateExerciseModule::RegisterMenus));
 
-	// 弹出的窗口
+	// 注册弹出的窗口 后续演示自定义窗口后 没用Tab的方式了
 	FGlobalTabmanager::Get()->RegisterNomadTabSpawner(SlateExerciseTabName, FOnSpawnTab::CreateRaw(this, &FSlateExerciseModule::OnSpawnPluginTab))
 		.SetDisplayName(LOCTEXT("FSlateExerciseTabTitle", "SlateExercise"))
 		.SetMenuType(ETabSpawnerMenuType::Hidden);
 
-	// 自定义弹出的窗口 准备制作有两个界面并排的窗口
+	// 注册自定义弹出的窗口 准备制作有两个界面并排的窗口
 	FGlobalTabmanager::Get()->RegisterNomadTabSpawner(MyWindowTabName1, FOnSpawnTab::CreateRaw(this, &FSlateExerciseModule::OnSpawnMyWindow1))
 		.SetDisplayName(LOCTEXT("FSlateExerciseWindowTitle1", "SlateExerciseWindow1"))
 		.SetMenuType(ETabSpawnerMenuType::Hidden);
@@ -73,8 +73,8 @@ void FSlateExerciseModule::StartupModule()
 	// 为了往扩展点里添加元素 先要拿到编辑器的模块
 	FLevelEditorModule& LevelEditorModule = FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor");
 
-	// 拓展时需要一系列的属性定义
-	TSharedPtr<FExtender> ToolBarExtender = MakeShareable(new FExtender); // 工具栏
+	// 拓展工具栏时需要一系列的属性定义
+	TSharedPtr<FExtender> ToolBarExtender = MakeShareable(new FExtender); 
 
 	// 拓展点的位置 填入位置 和命令 以及回调函数
 	ToolBarExtender->AddToolBarExtension("Play", EExtensionHook::After, PluginCommands, FToolBarExtensionDelegate::CreateRaw(this, &FSlateExerciseModule::AddToolbarExtension));
@@ -82,7 +82,8 @@ void FSlateExerciseModule::StartupModule()
 	// 将当前拓展 添加到工具栏的拓展管理器中
 	LevelEditorModule.GetToolBarExtensibilityManager()->AddExtender(ToolBarExtender);
 
-	// 从这里开始是拓展菜单栏的示例
+	
+	// 拓展菜单栏
 	TSharedPtr<FExtender> MenuBarExtender = MakeShareable(new FExtender); // 菜单栏
 	MenuBarExtender->AddMenuBarExtension("Help",EExtensionHook::After,PluginCommands,FMenuBarExtensionDelegate::CreateRaw(this,&FSlateExerciseModule::AddMenuBarExtension));
 
@@ -162,7 +163,17 @@ TSharedRef<class SDockTab> FSlateExerciseModule::OnSpawnMyWindow1(const class FS
 
 void FSlateExerciseModule::PluginButtonClicked()
 {
-	FGlobalTabmanager::Get()->TryInvokeTab(SlateExerciseTabName);
+	// 原本的方式 唤起一个Tab
+	//  FGlobalTabmanager::Get()->TryInvokeTab(SlateExerciseTabName);
+
+	auto NewWindow = SNew(SWindow)
+		.Title(FText::FromString("MyCustomWindow")) // 标题
+		.ClientSize(FVector2d(600,600))  // 大小
+		[
+			// 窗口中填充的内容
+			SNew(SSpacer)
+		];
+	FSlateApplication::Get().AddWindow(NewWindow);
 }
 
 void FSlateExerciseModule::RegisterMenus()
